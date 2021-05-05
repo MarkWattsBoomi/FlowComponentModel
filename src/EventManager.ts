28820667da08208e9233e01e008eed31dd67cf09
Mark Watts <42679182+MarkWattsBoomi@users.noreply.github.com>
2021-05-05 08:54:04 +0100

    added join and init event manager listeners

--- src/EventManager.ts
+++ src/EventManager.ts
@@ -1,18 +1,34 @@
 declare const manywho: any;
+declare const culture: any;
 
 if (!(manywho as any).eventManager) {
     (manywho as any).eventManager = {};
     (manywho as any).eventManager.beforeSendListeners = {};
     (manywho as any).eventManager.doneListeners = {};
+    (manywho as any).eventManager.initializedListeners = {};
+    (manywho as any).eventManager.joinListeners = {};
     (manywho as any).eventManager.failListeners = {};
     (manywho as any).eventManager.outcomeBeingTriggered;
     (manywho as any).eventManager.history = [];
 
     (manywho as any).eventManager.beforeSend = (xhr: XMLHttpRequest, request: any) => {
+        if(xhr) {
+            if(window.hasOwnProperty("culture") && culture.length > 0) {
+                xhr.setRequestHeader("Culture", culture); //"Brand=XL&Country=XL&Language=ES&Variant=XL");
+            }
+        }
         //(manywho as any).eventManager.beforeSendListeners.forEach((listener: any) => listener(xhr, request));
         for(const key in (manywho as any).eventManager.beforeSendListeners )
         {
             (manywho as any).eventManager.beforeSendListeners[key](xhr, request);
+        }
+    };
+
+    (manywho as any).eventManager.join = (xhr: XMLHttpRequest, request: any) => {
+        //(manywho as any).eventManager.doneListeners.forEach((listener: any) => listener(xhr, request));
+        for(const key in (manywho as any).eventManager.joinListeners )
+        {
+            (manywho as any).eventManager.joinListeners[key](xhr, request);
         }
     };
 
@@ -21,6 +37,14 @@ if (!(manywho as any).eventManager) {
         for(const key in (manywho as any).eventManager.doneListeners )
         {
             (manywho as any).eventManager.doneListeners[key](xhr, request);
+        }
+    };
+
+    (manywho as any).eventManager.initialized = (xhr: XMLHttpRequest, request: any) => {
+        //(manywho as any).eventManager.doneListeners.forEach((listener: any) => listener(xhr, request));
+        for(const key in (manywho as any).eventManager.initializedListeners )
+        {
+            (manywho as any).eventManager.initializedListeners[key](xhr, request);
         }
     };
 
@@ -38,6 +62,22 @@ if (!(manywho as any).eventManager) {
 
     (manywho as any).eventManager.removeBeforeSendListener = (componentId: string) => {
         delete (manywho as any).eventManager.beforeSendListeners[componentId];
+    };
+
+    (manywho as any).eventManager.addInitializedListener = (handler: (xhr: XMLHttpRequest, request: any) => void, componentId: string) => {
+        (manywho as any).eventManager.initializedListeners[componentId] = handler;
+    };
+
+    (manywho as any).eventManager.removeInitializedListener = (componentId: string) => {
+        delete (manywho as any).eventManager.initializedListeners[componentId];
+    };
+
+    (manywho as any).eventManager.addJoinListener = (handler: (xhr: XMLHttpRequest, request: any) => void, componentId: string) => {
+        (manywho as any).eventManager.joinListeners[componentId] = handler;
+    };
+
+    (manywho as any).eventManager.removeJoinListener = (componentId: string) => {
+        delete (manywho as any).eventManager.joinListeners[componentId];
     };
 
     (manywho as any).eventManager.addDoneListener = (handler: (xhr: XMLHttpRequest, request: any) => void, componentId: string) => {
@@ -64,12 +104,12 @@ if (!(manywho as any).eventManager) {
         },
         initialization: {
             beforeSend: (manywho as any).eventManager.beforeSend,
-            done: (manywho as any).eventManager.done,
+            done: (manywho as any).eventManager.initialized,
             fail: (manywho as any).eventManager.fail,
         },
         join: {
             beforeSend: (manywho as any).eventManager.beforeSend,
-            done: (manywho as any).eventManager.done,
+            done: (manywho as any).eventManager.join,
             fail: (manywho as any).eventManager.fail,
         },
     });
